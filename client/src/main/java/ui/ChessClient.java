3bbfe29f54fe3fb3ba6a6202e1778684c8b986ba
emchase8 <emchase8@byu.edu>
2025-11-11 15:56:45 -0700

    client can run (in theory but can't do anything)

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,9 +1,13 @@
 package ui;
 
 import sharedService.ServerFacade;
+import ui.ClientState;
+
+import java.util.Scanner;
 
 public class ChessClient {
     private final ServerFacade facade;
+    private ClientState currentState = ClientState.PRELOGIN;
 
     public ChessClient(String serverURL) {
         facade = new ServerFacade(serverURL);
@@ -11,6 +15,42 @@ public class ChessClient {
 
     public void run() {
         System.out.println("Welcome to the magical chess server! Please login or register to begin!");
+        System.out.print(help());
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
 
+        }
+    }
+    public String help() {
+        if (currentState == ClientState.PRELOGIN) {
+            return """
+                    Here are your current options:
+                    - to register yourself as a user and login: register <username> <password> <email>
+                    - to login as an existing user: login <username> <password>
+                    - to list all possible options: help
+                    - to quit the program: quit
+                    """;
+        } else if (currentState == ClientState.POSTLOGIN) {
+            return """
+                    Here are your current options:
+                    - to logout: logout
+                    - to create a chess game: create <game name>
+                    - to list all current chess games: list
+                    - to join a current game: join <game id> <WHITE or BLACK>
+                    - to observe a current game: observe <game id>
+                    - to list all possible options: help
+                    - to quit the program: quit
+                    """;
+        } else if (currentState == ClientState.GAMEPLAY){
+            return """
+                    Gameplay is still under construction, thank you for your patience.
+                    Here is what you can currently do:
+                    - to list all possible options: help
+                    - to leave gameplay: quitGame
+                    - to quit the program: quit
+                    """;
+        }
+        return "I'm not sure how you got here. Please contact the dev";
     }
 }
---
+++ client/src/main/java/ui/ClientState.java
@@ -0,0 +1,7 @@
+package ui;
+
+public enum ClientState {
+    PRELOGIN,
+    POSTLOGIN,
+    GAMEPLAY,
+}
