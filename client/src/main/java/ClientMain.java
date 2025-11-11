812cb6533fd62775f7adb7190ccd388999b389d0
emchase8 <emchase8@byu.edu>
2025-11-10 17:46:33 -0700

    setup for phase 5

--- client/src/main/java/ClientMain.java
+++ client/src/main/java/ClientMain.java
@@ -1,6 +1,7 @@
 import chess.*;
 
-public class Main {
+//make sure to make it able to run multiple clients
+public class ClientMain {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
